f84fedaeeae10a44481d3ca3c8b4adb5a8f2188e
MatthiasJeneW <matthias1.jenewein@gmail.com>
2023-06-07 14:57:16 +0200

    add Repository

---
+++ src/main/java/com/example/slmuebung/SlmuebungApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmuebung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmuebungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmuebungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmuebung/SlmuebungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmuebung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmuebungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
